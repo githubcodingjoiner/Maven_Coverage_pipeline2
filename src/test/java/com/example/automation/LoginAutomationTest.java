de893e99ddacf01f361c90cce129949783f8a629
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2025-01-02 09:42:30 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,39 +12,39 @@ public class LoginAutomationTest {
 
     @Test
     public void testLogin() {
-        // Set the path to Edge WebDriver (msedgedriver.exe)
+        // Set the path to Edge WebDriver
         System.setProperty("webdriver.edge.driver", "C:\\Program Files\\msedgedriver.exe");
 
-        // Initialize Edge WebDriver
-        WebDriver driver = new EdgeDriver();
+        // Configure EdgeOptions to avoid session issues
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        // Navigate to "The Internet" login page
-        driver.get("https://the-internet.herokuapp.com/login");
+        // Initialize Edge WebDriver with options
+        WebDriver driver = new EdgeDriver(options);
 
-        // Find username field and enter username
-        WebElement usernameField = driver.findElement(By.id("username"));
-        usernameField.sendKeys("tomsmith");
-
-        // Find password field and enter password
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("SuperSecretPassword!");
-
-        // Find and click the login button
-        WebElement loginButton = driver.findElement(By.className("radius"));
-        loginButton.click();
-
-        // Wait for the next page to load (can be replaced with WebDriverWait for better synchronization)
         try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Navigate to the login page
+            driver.get("https://the-internet.herokuapp.com/login");
+
+            // Interact with the login form
+            WebElement usernameField = driver.findElement(By.id("username"));
+            usernameField.sendKeys("tomsmith");
+
+            WebElement passwordField = driver.findElement(By.id("password"));
+            passwordField.sendKeys("SuperSecretPassword!");
+
+            WebElement loginButton = driver.findElement(By.className("radius"));
+            loginButton.click();
+
+            // Assert success message
+            WebElement successMessage = driver.findElement(By.id("flash"));
+            assertTrue(successMessage.getText().contains("You logged into a secure area!"));
+        } finally {
+            // Close the browser
+            driver.quit();
         }
-
-        // Assert the success message on the next page
-        WebElement successMessage = driver.findElement(By.id("flash"));
-        assertTrue(successMessage.getText().contains("You logged into a secure area!"));
-
-        // Close the driver
-        driver.quit();
     }
 }
