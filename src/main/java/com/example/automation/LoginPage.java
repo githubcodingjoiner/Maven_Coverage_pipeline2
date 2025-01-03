fb077d7d54ad1b9d51500e1b76032553832f7763
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2025-01-03 09:46:43 +0530

    Update LoginPage.java

--- src/main/java/com/example/automation/LoginPage.java
+++ src/main/java/com/example/automation/LoginPage.java
@@ -2,7 +2,6 @@ package com.example.automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
 @Component
