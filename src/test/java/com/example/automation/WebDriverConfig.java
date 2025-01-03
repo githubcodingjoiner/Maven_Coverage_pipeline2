b5c3a8fa90ee1fcf6fa699e2cbb9da6469f12386
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2025-01-03 09:48:47 +0530

    Create WebDriverConfig.java

---
+++ src/test/java/com/example/automation/WebDriverConfig.java
@@ -0,0 +1,21 @@
+package com.example.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebDriverConfig {
+
+    @Bean
+    public WebDriver webDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        return new ChromeDriver(options);
+    }
+}
